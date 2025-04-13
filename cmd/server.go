0a3881ae8d8143dc48996d56d8f9985770cead43
jadefox10200 <jadefox10200@users.noreply.github.com>
2025-04-13 13:04:04 -0700

    Create server.go
    add server.go

---
+++ cmd/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"zcomm/core"
+)
+
+var inbox = make(map[string][]core.ZMessage)
+
+func handleSend(w http.ResponseWriter, r *http.Request) {
+	var msg core.ZMessage
+	err := json.NewDecoder(r.Body).Decode(&msg)
+	if err != nil {
+		http.Error(w, "invalid message", http.StatusBadRequest)
+		return
+	}
+	inbox[msg.To] = append(inbox[msg.To], msg)
+	fmt.Fprintf(w, "Message delivered")
+}
+
+func handleReceive(w http.ResponseWriter, r *http.Request) {
+	user := r.URL.Query().Get("id")
+	msgs := inbox[user]
+	json.NewEncoder(w).Encode(msgs)
+	inbox[user] = []core.ZMessage{} // clear inbox after delivery
+}
+
+func main() {
+	http.HandleFunc("/send", handleSend)
+	http.HandleFunc("/receive", handleReceive)
+	
+	http.HandleFunc("/publish", handlePublishKeys)
+	http.HandleFunc("/pubkey", handleFetchKeys)
+
+
+	log.Println("ZComm Switchboard server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
